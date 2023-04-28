eb493d603eb6b2cb88974b6d916a782a390b7617
Anzhen <599002514@qq.com>
2023-04-29 01:17:50 +0800

    feat:后端项目初始化

---
+++ points-mall-system/points-mall-service/src/main/java/xyz/ziang/pointsmallservice/PointsMallServiceApplication.java
@@ -0,0 +1,13 @@
+package xyz.ziang.pointsmallservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PointsMallServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PointsMallServiceApplication.class, args);
+    }
+
+}
---
+++ points-mall-system/points-mall-service/src/test/java/xyz/ziang/pointsmallservice/PointsMallServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.ziang.pointsmallservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PointsMallServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
